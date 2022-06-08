eb237222bc70c51acac905bd04b3825ca6845e24
P <82906149+P-0001@users.noreply.github.com>
2022-06-08 12:07:17 -0700

    x
    x

--- assets/js/clip.js
+++ assets/js/clip.js
@@ -1,31 +1,51 @@
-const ele = document.getElementById('msg')
-
 function updateMessage(Msg = "Test") {
-    ele.innerText = Msg
+  document.getElementById("msg").innerText = Msg;
+}
+
+async function writeData(data) {
+  for (let name in data) {
+    const value = data[name];
+    const input = document.createElement("div");
+    input.innerText = `${name}: ${value}`;
+  }
 }
 
 async function writeClipboard() {
-    const urlSearchParams = new URLSearchParams(window.location.search);
-    const { email, password } = Object.fromEntries(urlSearchParams.entries());
-    const toCopy = [email, password].join(':')
+  const urlSearchParams = new URLSearchParams(window.location.search);
+  const { email, password, text } = Object.fromEntries(
+    urlSearchParams.entries()
+  );
+  let toCopy = "";
+  if (email && password) {
+    toCopy = [email, password].join(":");
+  } else {
+    toCopy = text || "";
+  }
 
-    navigator.permissions.query({ name: 'clipboard-read' }).then(result => {
-        // If permission to read the clipboard is granted or if the user will
-        // be prompted to allow it, we proceed.
-        if (result.state === 'granted' || result.state === 'prompt') {
-            navigator.clipboard.writeText(toCopy)
-                .then(() => updateMessage('Copied!'))
-                .catch(err => {
-                    console.error('Failed to read clipboard contents: ', err);
-                    if (navigator.clipboard)
-                        updateMessage('Failed. Please Focus Tab And Reload.')
-                    else {
-                        updateMessage('Clipboard In Your Browser Not Allowed/Available')
-                    }
-                });
-        }
-    })
-
+  navigator.permissions.query({ name: "clipboard-read" }).then((result) => {
+    if (result.state === "granted" || result.state === "prompt") {
+      navigator.clipboard
+        .writeText(toCopy)
+        .then(() => updateMessage(`Copied! ${toCopy}`))
+        .catch((err) => {
+          console.error("Failed to read clipboard contents: ", err);
+          if (navigator.clipboard)
+            updateMessage("Failed. Please Focus Tab And Reload.");
+          else {
+            updateMessage("Clipboard In Your Browser Not Allowed/Available");
+          }
+        });
+    }
+  });
 }
 
-writeClipboard()
+if (window.location.search && window.location.search.includes("data")) {
+  const urlSearchParams = new URLSearchParams(window.location.search);
+  const { data } = Object.fromEntries(urlSearchParams.entries());
+  let real = atob(data);
+  writeData(JSON.parse(real));
+} else {
+  const urlSearchParams = new URLSearchParams(window.location.search);
+  const q = Object.fromEntries(urlSearchParams.entries());
+  if (q) writeClipboard();
+}
