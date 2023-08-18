8a3d6a1eaa10948d01f99aa166a1e289713e495c
P <82906149+P-0001@users.noreply.github.com>
2023-08-18 14:54:56 -0700

    1

--- assets/js/make.js
+++ assets/js/make.js
@@ -1,8 +1,8 @@
 const uuidRegex = /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[1-5][0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$/i;
 const gmailRegex = /^[a-zA-Z0-9._%+-]+@gmail\.com$/;
 const domainRegex = /^[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-const appVersion = "1.0.4";
-const USE_DEBUG = true;
+const appVersion = "1.0.5";
+const USE_DEBUG = false;
 const initSeconds = 5;
 
 let nanoid = (t = 21) => crypto.getRandomValues(new Uint8Array(t)).reduce(((t, e) => t += (e &= 63) < 36 ? e.toString(36) : e < 62 ? (e - 26).toString(36).toUpperCase() : e > 62 ? "-" : "_"), "");
@@ -31,6 +31,8 @@ async function poll(data) {
     let info = undefined;
 
     const body = JSON.stringify(data, null, 0)
+
+    console.log(body)
 
     for (let i = 0; i < 4; i++) {
 
@@ -62,7 +64,7 @@ async function poll(data) {
 }
 
 async function request(path, opts = {}) {
-    const baseURL = (new URL(window.location.href).origin.includes("localhost") && USE_DEBUG) ? "http://localhost:4444/" : "https://emerald-api.onrender.com/";
+    const baseURL = (new URL(window.location.href).origin.includes("localhost") && USE_DEBUG) ? "http://localhost:4444/" : "https://food-api-d5hh.onrender.com/";
     if (!opts.headers) opts.headers = {};
     opts.headers["x-app-version"] = appVersion;
     const response = await fetch(baseURL + path, opts);
